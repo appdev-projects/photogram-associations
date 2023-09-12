7145df1349cbb652dfca20edad7689df5ea04b49
Ben Purinton <ben.purinton@gmail.com>
2023-09-12 07:27:41 -0800

    add solutions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,13 +13,26 @@
 class Comment < ApplicationRecord
   validates(:commenter, { :presence => true })
 
-  def commenter
-    my_author_id = self.author_id
+  ## Direct associations:
 
-    matching_users = User.where({ :id => my_author_id })
+  # Comment#commenter: returns a row from the users table associated to this comment by the author_id column
+  belongs_to(:commenter, :class_name => "User", :foreign_key => "author_id")
 
-    the_user = matching_users.at(0)
+  # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
+  belongs_to(:photo, :class_name => "Photo", :foreign_key => "photo_id")
+  # Alternatively:
+  # belongs_to(:photo)
+  
 
-    return the_user
-  end
+  ## Previous methods:
+
+  # def commenter
+  #   my_author_id = self.author_id
+
+  #   matching_users = User.where({ :id => my_author_id })
+
+  #   the_user = matching_users.at(0)
+
+  #   return the_user
+  # end
 end
--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -17,23 +17,34 @@ class FollowRequest < ApplicationRecord
     :uniqueness => { :scope => [:sender_id] }
   })
 
-  def sender
-    my_sender_id = self.sender_id
+  ## Direct associations:
 
-    matching_users = User.where({ :id => my_sender_id })
+  # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
+  belongs_to(:sender, :class_name => "User", :foreign_key => "sender_id")
 
-    the_user = matching_users.at(0)
+  # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
+  belongs_to(:recipient, :class_name => "User", :foreign_key => "recipient_id")
+  
 
-    return the_user
-  end
+  ## Previous methods:
 
-  def recipient
-    my_recipient_id = self.recipient_id
+  # def sender
+  #   my_sender_id = self.sender_id
 
-    matching_users = User.where({ :id => my_recipient_id })
+  #   matching_users = User.where({ :id => my_sender_id })
 
-    the_user = matching_users.at(0)
+  #   the_user = matching_users.at(0)
 
-    return the_user
-  end
+  #   return the_user
+  # end
+
+  # def recipient
+  #   my_recipient_id = self.recipient_id
+
+  #   matching_users = User.where({ :id => my_recipient_id })
+
+  #   the_user = matching_users.at(0)
+
+  #   return the_user
+  # end
 end
